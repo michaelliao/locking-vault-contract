5dd115a4473d3767b99f2b3d517508ce8cd78d54
Michael Liao <askxuefeng@gmail.com>
2022-11-30 19:39:43 +0800

    update contract

--- contracts/LockingVault.sol
+++ contracts/LockingVault.sol
@@ -5,7 +5,7 @@ pragma solidity =0.8.17;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-contract LockVault {
+contract LockingVault {
     using EnumerableSet for EnumerableSet.UintSet;
     using SafeERC20 for IERC20;
 
